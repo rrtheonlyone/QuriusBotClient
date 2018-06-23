d652d12fc1bd216b6ffbbf34a2eb8b33b5f8fb9e
Rahul Rajesh <rahul.rajesh@hotmail.com>
2018-06-23 23:22:44 +0800

    Worked on js query to send to API

--- popup.js
+++ popup.js
@@ -1,21 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
+var checkPageButton = document.getElementById('checkPage');
+var API_URL = "localhost:5000"
 
-    chrome.tabs.getSelected(null, function(tab) {
-      d = document;
+chrome.tabs.executeScript( 
+  null, {
+    code: "document.documentElement.innerHTML;"
+  },
+  
+  function(results){ 
+    console.log(results);
 
-      var f = d.createElement('form');
-      f.action = 'http://gtmetrix.com/analyze.html?bm';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
-      i.value = tab.url;
-      f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
-    });
-  }, false);
-}, false);
+    
+
+  } 
+);
+
 
