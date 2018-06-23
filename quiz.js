36d8385c2b5507695d103a26391af82d2e36d6ce
Rahul Rajesh <rahul.rajesh@hotmail.com>
2018-06-24 07:48:09 +0800

    updated quiz

--- quiz.js
+++ quiz.js
@@ -1,4 +1,6 @@
 var API_BASE_URL = "http://localhost:5000"
+var questions = [];
+var id = 0;
 
 function getDataForQuiz(html) {
   var settings = {
@@ -19,6 +21,23 @@ chrome.tabs.executeScript(
   function(results){ 
     getDataForQuiz(results[0]).then((data) => {
       console.log(data);
+      questions = data["data"];
+      display_question();
     });
   } 
 );
+
+function display_question() {
+  console.log(id)
+  question_data = JSON.parse(questions[id]);
+  document.getElementById("question").innerHTML = question_data.question
+  	.replace(/\".*\n|\n.*\"/g, "");
+  document.getElementById("answer").innerHTML = question_data.answer;
+  id++;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	$('#nextButton').on('click', function(event) {
+	  display_question()
+	});
+});
