30e92f7a5a6ac092d42d0256753b477b6c8b6283
Ju <jluuiss@gmail.com>
2018-06-24 10:55:58 +0800

    Add caching

--- quiz.js
+++ quiz.js
@@ -1,3 +1,36 @@
+function fetchQuestions(html, url, callback) {
+  chrome.storage.local
+    .get(['questions', 'questionsCachedAt', 'questionsURL'],
+      (questionsObj) => {
+        if (
+          questionsObj.questionsURL == url &&
+          questionsObj.questions != undefined &&
+          questionsObj.questionsCachedAt != undefined &&
+          questionsObj.questionsCachedAt > Date.now() - 3600*1000
+        ) {
+          callback(questionsObj.questions)
+        } else {
+          var settings = {
+              "async": true,
+              "crossDomain": true,
+              "url": API_BASE_URL + '/quiz',
+              "method": "POST",
+              "data": {'data': html}
+          }
+          $.ajax(settings).then(response => {
+            chrome.storage.local.set({
+              questionsURL: url,
+              questions: response,
+              questionsCachedAt: Date.now(),
+            })
+            console.log(response);
+            callback(response);
+          });
+        }
+      }
+    )
+}
+
 var API_BASE_URL = "http://localhost:5000"
 var questions = [];
 var id = 0;
@@ -19,19 +52,20 @@ function getDataForQuiz(html) {
   return $.ajax(settings);
 }
 
-chrome.tabs.executeScript( 
+chrome.tabs.executeScript(
   null, {
-    code: "document.documentElement.innerHTML;"
+    code: "[document.documentElement.innerHTML, document.location.href];"
   },
-  
-  function(results){ 
+
+  function(results){
     $(".front").html(`<div style="display:flex; justify-content:center;"><div class="loader"/></div>`);
-    getDataForQuiz(results[0]).then((data) => {
-      console.log(data);
+    fetchQuestions(results[0][0], results[0][1], (data) => {
       questions = data["data"];
+      // console.log(data);
+      // console.log(questions);
       display_question();
     });
-  } 
+  }
 );
 
 function display_question() {
