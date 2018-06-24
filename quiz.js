e33955072dbdff6b6a44bb5d6bf1ca874f09db08
Sreyans Sipani <sreyans.sipani99@gmail.com>
2018-06-24 10:01:28 +0800

    Handle case when no questions

--- quiz.js
+++ quiz.js
@@ -1,7 +1,7 @@
 var API_BASE_URL = "http://localhost:5000"
 var questions = [];
 var id = 0;
-var new_id = 5;
+var next_id = 5;
 
 function getDataForQuiz(html) {
   var settings = {
@@ -29,17 +29,18 @@ chrome.tabs.executeScript(
 );
 
 function display_question() {
-  if(new_id <= id) {
+  if(next_id <= id || questions.length == 0) {
     document.getElementById("question").innerHTML = "No more questions left";
     document.getElementById("answer").innerHTML = "No answer";
+  } else {
+    question_data = JSON.parse(questions[id]);
+    question = question_data.question.replace(/(\\n)+/g, '');
+    question = question.replace(/(\\")+/g, '');
+    document.getElementById("question").innerHTML = question;
+    document.getElementById("answer").innerHTML = question_data.answer;
+    id = (id + 5) & questions.length;
+    next_id = (next_id + 5) % questions.length;
   }
-  question_data = JSON.parse(questions[id]);
-  question = question_data.question.replace(/(\\n)+/g, '');
-  question = question.replace(/(\\")+/g, '');
-  document.getElementById("question").innerHTML = question;
-  document.getElementById("answer").innerHTML = question_data.answer;
-  id = (id + 5) & questions.length;
-  new_id = (new_id + 5) % questions.length;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
